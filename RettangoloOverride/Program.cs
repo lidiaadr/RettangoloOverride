6830df86edfced3a0c9c8919418d34edd34d2f90
acer <acer@LAPTOP-H9QES7A9>
2024-01-24 20:00:51 +0100

    Aggiungere i file di progetto.

---
+++ RettangoloOverride/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RettangoloOverride
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float l1 = float.Parse(Console.ReadLine());
+            float l2 = float.Parse(Console.ReadLine());
+            Rettangolo r = new Rettangolo (l2,l1);
+            Console.WriteLine(r.Area());
+            Console.WriteLine(r.Perimetro());
+            Console.WriteLine(r.ToString());
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ RettangoloOverride/Rettangolo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RettangoloOverride
+{
+    internal class Rettangolo
+    {
+        public float Lato1 { get; set; }
+        public float Lato2 { get; set; }
+
+        public Rettangolo(float lato1, float lato2)
+        {
+            Lato1 = lato1;
+            Lato2 = lato2;
+        }
+
+        public float Area()
+        {
+            return (Lato1 * Lato2) / 2;
+        }
+        public float Perimetro()
+        {
+            return (2 * Lato1) + (2 * Lato2);
+        }
+        public override string ToString()
+        {
+            return string.Format("{0}", Lato1, Lato2);
+        }
+    }
+}
